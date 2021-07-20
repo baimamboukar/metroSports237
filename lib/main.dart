8e42ca07371681a382d4e93f72ca86e0ae004ad1
baimamboukar <baimamboukar@gmail.com>
2021-07-20 18:49:42 +0100

    details screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:line_icons/line_icons.dart';
@@ -13,7 +14,7 @@ void main() {
 class MetroSports237 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
         theme: ThemeData(
             primarySwatch: Colors.green,
             textTheme:
--- lib/widgets/article_trailer.dart
+++ lib/widgets/article_trailer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:metro_sports/models/models.dart';
+import 'package:metro_sports/screens/screens.dart';
 
 class ArticleTrailer extends StatelessWidget {
   final Article article;
@@ -8,29 +10,32 @@ class ArticleTrailer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListTile(
-          leading: Container(
-            height: 120.0,
-            width: 100.0,
-            child: FlutterLogo(
-              style: FlutterLogoStyle.stacked,
+    return GestureDetector(
+      onTap: () => Get.to(() => ArticleDeatils(article: article)),
+      child: Container(
+        child: ListTile(
+            leading: Container(
+              height: 120.0,
+              width: 100.0,
+              child: FlutterLogo(
+                style: FlutterLogoStyle.stacked,
+              ),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.green, width: .45),
+                  borderRadius: BorderRadius.circular(20.0)),
             ),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.green, width: .45),
-                borderRadius: BorderRadius.circular(20.0)),
-          ),
-          title: Text("Leicester confirms siging a player from Salzbourg",
-              maxLines: 2, overflow: TextOverflow.ellipsis),
-          subtitle: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("2021 Jul 19"),
-              Row(
-                children: [Icon(LineIcons.fire)],
-              )
-            ],
-          )),
+            title: Text("Leicester confirms siging a player from Salzbourg",
+                maxLines: 2, overflow: TextOverflow.ellipsis),
+            subtitle: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("2021 Jul 19"),
+                Row(
+                  children: [Icon(LineIcons.fire)],
+                )
+              ],
+            )),
+      ),
     );
   }
 }
