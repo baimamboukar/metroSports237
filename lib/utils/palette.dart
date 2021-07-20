e65cace6ed9831850020709da6e2976ea7ed81b3
baimamboukar <baimamboukar@gmail.com>
2021-07-20 18:15:10 +0100

    files added in utils

---
+++ lib/utils/palette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static Color primary = Colors.green;
+  static Color secondary = Colors.red;
+  static Color success = Colors.green;
+  static Color error = Colors.redAccent;
+  static Color light = Colors.white;
+  static Color dark = Colors.black;
+}
